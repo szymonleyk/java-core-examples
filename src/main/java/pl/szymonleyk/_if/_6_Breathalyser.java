87ad541487f071574217de510a5feb5cfcb328b0
Szymon Leyk <biuro@szymonleyk.pl>
2024-04-22 19:53:38 +0200

    add 6th example - Breathalyser

---
+++ src/main/java/pl/szymonleyk/_if/_6_Breathalyser.java
@@ -0,0 +1,23 @@
+package pl.szymonleyk._if;
+
+import java.util.Scanner;
+
+/**
+ * This program ask you, how many beers you have drunk and tell you if can/can't drive car.
+ * 0-5 ? YES
+ * 5 - more ? NO
+ */
+public class _6_Breathalyser {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.print("How many beers have you drunk? ");
+        var drunkBeer = scanner.nextInt();
+
+        if(drunkBeer <= 5) {
+            System.out.println("OK, you can drive.");
+        } else {
+            System.out.println("Sorry, you can't drive.");
+        }
+    }
+}
